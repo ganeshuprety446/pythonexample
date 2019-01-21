195187ca7a553edf663d294926dba12f9d517ca4
Ganesh Uprety <33591508+ganeshuprety446@users.noreply.github.com>
2019-01-21 16:26:08 +0530

    Update additionoftwonumber.py

--- additionoftwonumber.py
+++ additionoftwonumber.py
@@ -2,4 +2,10 @@
 a = int(input("enter first number: "))
 b = int(input("enter second number: "))
 sum = a + b
+mul=a*b
+div=a/b
+sub=a-b
 print("sum:", sum)
+print("div:", div)
+print("mul:", mul)
+print("sub:", sub)
