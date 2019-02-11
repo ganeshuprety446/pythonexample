14945f1c5432f203f88c68a64dbad643a6a0e46d
Ganesh Uprety <33591508+ganeshuprety446@users.noreply.github.com>
2019-02-11 15:27:13 +0530

    Create relationaloperator.py

---
+++ relationaloperator.py
@@ -0,0 +1,25 @@
+a = input("enter no first")
+b = input("enter number second")
+print("first number is",a)
+print("second no is ",b)
+
+if ( a == b ):
+   print (" a is equal to b")
+else:
+   print (" a is not equal to b")
+
+if ( a != b ):
+   print (" a is not equal to b")
+else:
+   print (" a is equal to b")
+
+if ( a < b ):
+   print (" a is less than b" )
+else:
+   print ("a is not less than b")
+
+if ( a > b ):
+   print (" a is greater than b")
+else:
+   print (" a is not greater than b")
+
