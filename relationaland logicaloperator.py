8ef37640db3febdebf2a15d3c6ee556dc08b5698
Ganesh Uprety <33591508+ganeshuprety446@users.noreply.github.com>
2019-02-11 15:48:45 +0530

    Update and rename relationaloperator.py to relationaland logicaloperator.py

--- relationaland logicaloperator.py
+++ relationaland logicaloperator.py
@@ -1,7 +1,11 @@
 a = input("enter no first")
 b = input("enter number second")
+username=input("enter username \n")
+password=input("enter password \n")
 print("first number is",a)
 print("second no is ",b)
+print("username  number is",username)
+print("password is ",password)
 
 if ( a == b ):
    print (" a is equal to b")
@@ -22,4 +26,10 @@ if ( a > b ):
    print (" a is greater than b")
 else:
    print (" a is not greater than b")
+
+if(username=="admin" and password=="1233"):
+ print("welcome to main page")
+else:
+ print("try again")
+
 
